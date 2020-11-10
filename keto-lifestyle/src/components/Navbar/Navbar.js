c26cc5f86fc1cbc27f6829b9470f5a27b9576525
Jesse Audije <audijej@gmail.com>
2020-11-09 23:17:48 -0800

    navbar added to landing page

---
+++ keto-lifestyle/src/components/Navbar/Navbar.js
@@ -0,0 +1,61 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./navbarStyle.css";
+
+
+function Navbar() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-dark">
+            <Link className="navbar-brand" to="/">
+                <h1>Keto Lifestyle</h1>
+            </Link>
+            <div>
+                <ul className="navbar-nav">
+                    <li className="nav-item">
+                        <Link
+                            id="navbarTab"
+                            to="/about"
+                            className={
+                                window.location.pathname === "/about" || window.location.pathname === "/about"
+                                    ? "nav-link active"
+                                    : "nav-link"
+                            }
+                        >
+                            About
+              </Link>
+                    </li>
+
+                    <li className="nav-item">
+                        <Link
+                            id="navbarTab"
+                            to="/portfolio"
+                            className={window.location.pathname === "/portfolio" ? "nav-link active" : "nav-link"}
+                        >
+                            Portfolio
+              </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            id="navbarTab"
+                            to="/photos"
+                            className={window.location.pathname === "/photos" ? "nav-link active" : "nav-link"}
+                        >
+                            Photographs
+              </Link>
+                    </li>
+                    <li className="nav-item">
+                        <Link
+                            id="navbarTab"
+                            to="/contact"
+                            className={window.location.pathname === "/contact" ? "nav-link active" : "nav-link"}
+                        >
+                            Contact
+              </Link>
+                    </li>
+                </ul>
+            </div>
+        </nav>
+    );
+}
+
+export default Navbar;
