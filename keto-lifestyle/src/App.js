a560ac75bb3a1dbc68a94878ce6ebef9eb2b04da
Jesse Audije <audijej@gmail.com>
2020-11-10 21:42:00 -0800

    update to css

--- keto-lifestyle/src/App.js
+++ keto-lifestyle/src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Navbar from './components/Navbar/Navbar';
+import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage'
 
 import './App.css';
@@ -9,10 +10,13 @@ function App() {
   return (
 
     <Router>
-    <div className="App">
-      <Navbar />
-      <Route exact path="/" component={LandingPage} />
-    </div>
+      <div className="App">
+        <Navbar />
+
+        <Route exact path="/" component={LandingPage} />
+<br></br>
+        <Footer />
+      </div>
     </Router>
   );
 }
--- keto-lifestyle/src/pages/LandingPage.js
+++ keto-lifestyle/src/pages/LandingPage.js
@@ -1,15 +1,18 @@
 import React, { Component } from "react";
 import { BrowserRouter as Router, Route } from "react-router-dom";
 import Jumbotron from 'react-bootstrap/Jumbotron';
+import Container from 'react-bootstrap/Container';
 import '../App.css'
 
 function LandingPage() {
-    return(
-        <div>
-<Jumbotron id='landingPageJumbotron'>
-    <h1>The Carnivore Life</h1>
-    <h2>Lifestyle | Recipes | Blog</h2>
-</Jumbotron>
+    return (
+        <div style={{ backgroundColor: 'black' }}>
+            <Container>
+                <Jumbotron id='landingPageJumbotron'>
+                    <h1>The Carnivore Life</h1>
+                    <h2>Lifestyle | Recipes | Blog</h2>
+                </Jumbotron>
+            </Container>
         </div>
     )
 }
