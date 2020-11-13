ec9a3d9ee29df31c77760479867e58e7352dfb2b
Jesse Audije <audijej@gmail.com>
2020-11-12 21:30:43 -0800

    adjusted jumbotron and css

--- keto-lifestyle/src/pages/LandingPage.js
+++ keto-lifestyle/src/pages/LandingPage.js
@@ -7,10 +7,14 @@ import '../App.css'
 function LandingPage() {
     return (
         <div style={{ backgroundColor: 'black' }}>
-                <Jumbotron id='landingPageJumbotron'>
+
+            <Jumbotron id='landingPageJumbotron' fluid>
+                <Container>
                     <h1>The Carnivore Life</h1>
-                    <h2>Lifestyle | Recipes | Blog</h2>
-                </Jumbotron>
+                    <h2><i class="fas fa-bacon"></i> Meat |Lifestyle | Recipes | Blog <i class="fas fa-drumstick-bite"></i></h2>
+                </Container>
+            </Jumbotron>
+
         </div>
     )
 }
