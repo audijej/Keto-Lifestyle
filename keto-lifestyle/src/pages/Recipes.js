1804a38360c77626dbab7c71c62ccb7f62a2bb6f
Jesse Audije <audijej@gmail.com>
2020-11-13 22:27:29 -0800

    recipe boiler layout

---
+++ keto-lifestyle/src/pages/Recipes.js
@@ -0,0 +1,67 @@
+import { Link, useParams } from "react-router-dom";
+import Jumbotron from 'react-bootstrap/Jumbotron';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+
+function RecipesPage() {
+    return (
+        <div>
+
+            <Jumbotron fluid>
+                <Container>
+                    <h1>Fluid jumbotron</h1>
+                    <p>
+                        This is a modified jumbotron that occupies the entire horizontal space of
+                        its parent.
+                    </p>
+                </Container>
+            </Jumbotron>
+
+            <CardGroup>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Pork Belly</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This content is a little bit longer.
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Wagyu Steak</Card.Title>
+                        <Card.Text>
+                            This card has supporting text below as a natural lead-in to additional
+                        content.{' '}
+                        </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+                <Card>
+                    <Card.Img variant="top" src="holder.js/100px160" />
+                    <Card.Body>
+                        <Card.Title>Spicy Salmon</Card.Title>
+                        <Card.Text>
+                            This is a wider card with supporting text below as a natural lead-in to
+                            additional content. This card has even longer content than the first to
+                            show that equal height action.
+      </Card.Text>
+                    </Card.Body>
+                    <Card.Footer>
+                        <small className="text-muted">Last updated 3 mins ago</small>
+                    </Card.Footer>
+                </Card>
+            </CardGroup>
+
+        </div>
+    )
+}
