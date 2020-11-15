28a16235121605ce6adcd8707eb6ea545c2092c1
Jesse Audije <audijej@gmail.com>
2020-11-14 22:55:01 -0800

    app.js route update for recipes

--- keto-lifestyle/src/App.js
+++ keto-lifestyle/src/App.js
@@ -4,6 +4,13 @@ import Navbar from './components/Navbar/Navbar';
 import Footer from './components/Footer/Footer';
 import LandingPage from './pages/LandingPage';
 import RecipesPage from './pages/Recipes';
+import Beef from './pages/Beef';
+import Breakfast from './pages/Breakfast';
+import Chicken from './pages/Chicken';
+import Lamb from './pages/Lamb';
+import Pork from './pages/Pork';
+import Seafood from './pages/Seafood';
+import Turkey from './pages/Turkey';
 
 import './App.css';
 
@@ -16,6 +23,13 @@ function App() {
 
         <Route exact path="/" component={LandingPage} />
         <Route exact path="/recipes" component={RecipesPage} />
+        <Route exact path="/beef" component={Beef} />
+        <Route exact path="/breakfast" component={Breakfast} />
+        <Route exact path="/chicken" component={Chicken} />
+        <Route exact path="/lamb" component={Lamb} />
+        <Route exact path="/pork" component={Pork} />
+        <Route exact path="/seafood" component={Seafood} />
+        <Route exact path="/turkey" component={Turkey} />
 <br></br>
         <Footer />
       </div>
